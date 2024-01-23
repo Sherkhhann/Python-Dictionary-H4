f681cd6ae1f0359da631ff51ccf0168dfd8332c8
Sherkhhann <sinomukhtorov13@gmail.com>
2024-01-23 18:21:52 +0500

    First File

---
+++ Task1.py
@@ -0,0 +1,14 @@
+# words=['p', 'q']
+# i=1
+# index=i
+# while i<=5:
+#  words.append(i)
+#  i+=1
+# print(words)
+
+a = int(input())
+number = a
+for i in range( number+1):
+ for j in range(1, i+1):
+  print(j, end=' ')
+print()
---
+++ Task2.py
@@ -0,0 +1,3 @@
+a = int(input())
+for i in [2,3,4,5,6,7,8,9,10]:
+ print(a*i)
